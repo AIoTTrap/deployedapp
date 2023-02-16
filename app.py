b3f45cec73086fb38816f57ad291a360fe4fa033
Manan Luthra <105019328+manan-luthra@users.noreply.github.com>
2023-02-16 09:58:35 -0700

    esp32post

--- app.py
+++ app.py
@@ -142,5 +142,46 @@ def upload_image():
             return Response(response=response, status=200, mimetype='image/png')
         except FileNotFoundError:
             abort(404)
+            
+@app.route('/esp32post', methods=['POST'])
+def upload_image():     
+    # image=request.files['file']
+    image = request.files.get("image", None)
+    if image:
+        image_name=secure_filename(image.filename)
+        #image = request.files["images"]
+        #image_name = image.filename
+        image.save(os.path.join(os.getcwd(), image_name))
+        img_raw = tf.image.decode_image(
+            open(image_name, 'rb').read(), channels=3)
+        img = tf.expand_dims(img_raw, 0)
+        img = transform_images(img, size)
+
+        t1 = time.time()
+        boxes, scores, classes, nums = yolo(img)
+        t2 = time.time()
+        print('time: {}'.format(t2 - t1))
+
+        print('detections:')
+        for i in range(nums[0]):
+            print('\t{}, {}, {}'.format(class_names[int(classes[0][i])],
+                                            np.array(scores[0][i]),
+                                            np.array(boxes[0][i])))
+        img = cv2.cvtColor(img_raw.numpy(), cv2.COLOR_RGB2BGR)
+        img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
+        # cv2.imwrite(output_path + 'detection.jpg', img)
+        # print('output saved to: {}'.format(output_path + 'detection.jpg'))
+        
+        # prepare image for response
+        _, img_encoded = cv2.imencode('.png', img)
+        response = img_encoded.tostring()
+        
+        #remove temporary image
+        os.remove(image_name)
+
+        try:
+            return Response(response=response, status=200, mimetype='image/png')
+        except FileNotFoundError:
+            abort(404)
 if __name__ == '__main__':
     app.run(debug=True, host = '0.0.0.0', port=5000)
