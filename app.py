a13da0b469a90909ba1acc1e176047cc445e48a5
Manan Luthra <105019328+manan-luthra@users.noreply.github.com>
2023-03-05 16:58:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ def upload_image():
             
 @app.route('/esp32post', methods=['GET','POST'])
 def uploadimage32():     
-    image=request.files['name']
+    image=request.files['filename']
     # image = request.files.get("image", None)
 
     image_name=secure_filename(image.filename)
