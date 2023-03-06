35cc7bf7a70458fa22bef8ec49009634935f2e3f
Manan Luthra <105019328+manan-luthra@users.noreply.github.com>
2023-03-05 17:39:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -158,7 +158,7 @@ def uploadimage32():
     # image = request.files.get("image", None)
 
     #image_name=secure_filename(image.filename)
-    image_name=secure_filename(image)
+    image_name=secure_filename(filename.filename)
 
     """ # if base64 image is sent instead
     if image is None:
