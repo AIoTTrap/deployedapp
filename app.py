0a603280e8c664389328d44f9d0f37b6b8cb0bf2
Manan Luthra <105019328+manan-luthra@users.noreply.github.com>
2023-04-11 15:36:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -867,7 +867,7 @@ def boundbox_test():
     Image.fromarray(image_np)
 
     RGB_img = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
-    RGB_img.save(os.path.join(app.config['UPLOAD_FOLDER'], image_name)))
+    RGB_img.save(os.path.join(app.config['UPLOAD_FOLDER'], image_name))
     #print(os.getcwd)
     # os.remove('static/uploads/out.jpg')
     # cv2.imwrite('static/uploads/out.jpg',RGB_img)
