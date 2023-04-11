25560f20b246763ef1faf7dc749f0cc5f72829ed
Manan Luthra <105019328+manan-luthra@users.noreply.github.com>
2023-04-11 13:14:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -878,4 +878,4 @@ def display_obj_test(filename):
     return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 if __name__ == '__main__':
-    app.run(debug=True, host = '0.0.0.0', port=5000)        
+    app.run(debug=True, use_reloader=False, host = '0.0.0.0', port=5000)        
